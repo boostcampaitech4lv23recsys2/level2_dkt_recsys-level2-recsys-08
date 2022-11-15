49371fcc98e22d76a8bfdddce330922d5c4e7863
netsus <cwjcsk@naver.com>
2022-11-16 00:28:28 +0900

    Initialize Baseline

---
+++ dkt/src/model.py
@@ -0,0 +1,225 @@
+import torch
+import torch.nn as nn
+
+try:
+    from transformers.modeling_bert import BertConfig, BertEncoder, BertModel
+except:
+    from transformers.models.bert.modeling_bert import (
+        BertConfig,
+        BertEncoder,
+        BertModel,
+    )
+
+
+class LSTM(nn.Module):
+    def __init__(self, args):
+        super(LSTM, self).__init__()
+        self.args = args
+
+        self.hidden_dim = self.args.hidden_dim
+        self.n_layers = self.args.n_layers
+
+        # Embedding
+        # interaction은 현재 correct로 구성되어있다. correct(1, 2) + padding(0)
+        self.embedding_interaction = nn.Embedding(3, self.hidden_dim // 3)
+        self.embedding_test = nn.Embedding(self.args.n_test + 1, self.hidden_dim // 3)
+        self.embedding_question = nn.Embedding(self.args.n_questions + 1, self.hidden_dim // 3)
+        self.embedding_tag = nn.Embedding(self.args.n_tag + 1, self.hidden_dim // 3)
+
+        # embedding combination projection
+        self.comb_proj = nn.Linear((self.hidden_dim // 3) * 4, self.hidden_dim) # 원하는 차원으로 줄이기
+
+        self.lstm = nn.LSTM(
+            self.hidden_dim, self.hidden_dim, self.n_layers, batch_first=True
+        )
+
+        # Fully connected layer
+        self.fc = nn.Linear(self.hidden_dim, 1)
+
+    def forward(self, input):
+
+        test, question, tag, _, mask, interaction = input
+
+        batch_size = interaction.size(0)
+
+        # Embedding
+        embed_interaction = self.embedding_interaction(interaction)
+        embed_test = self.embedding_test(test)
+        embed_question = self.embedding_question(question)
+        embed_tag = self.embedding_tag(tag)
+
+        embed = torch.cat(
+            [
+                embed_interaction,
+                embed_test,
+                embed_question,
+                embed_tag,
+            ],
+            2,
+        )
+
+        X = self.comb_proj(embed)
+
+        out, _ = self.lstm(X)
+        out = out.contiguous().view(batch_size, -1, self.hidden_dim)
+        out = self.fc(out).view(batch_size, -1)
+        return out
+
+
+class LSTMATTN(nn.Module):
+    def __init__(self, args):
+        super(LSTMATTN, self).__init__()
+        self.args = args
+
+        self.hidden_dim = self.args.hidden_dim
+        self.n_layers = self.args.n_layers
+        self.n_heads = self.args.n_heads
+        self.drop_out = self.args.drop_out
+
+        # Embedding
+        # interaction은 현재 correct로 구성되어있다. correct(1, 2) + padding(0)
+        self.embedding_interaction = nn.Embedding(3, self.hidden_dim // 3)
+        self.embedding_test = nn.Embedding(self.args.n_test + 1, self.hidden_dim // 3)
+        self.embedding_question = nn.Embedding(
+            self.args.n_questions + 1, self.hidden_dim // 3
+        )
+        self.embedding_tag = nn.Embedding(self.args.n_tag + 1, self.hidden_dim // 3)
+
+        # embedding combination projection
+        self.comb_proj = nn.Linear((self.hidden_dim // 3) * 4, self.hidden_dim)
+
+        self.lstm = nn.LSTM(
+            self.hidden_dim, self.hidden_dim, self.n_layers, batch_first=True
+        )
+
+        ## 이 부분도 Attention에 의해 추가된 부분
+        self.config = BertConfig(
+            3,  # not used
+            hidden_size=self.hidden_dim,
+            num_hidden_layers=1,
+            num_attention_heads=self.n_heads,
+            intermediate_size=self.hidden_dim,
+            hidden_dropout_prob=self.drop_out,
+            attention_probs_dropout_prob=self.drop_out,
+        )
+        self.attn = BertEncoder(self.config)
+
+        # Fully connected layer
+        self.fc = nn.Linear(self.hidden_dim, 1)
+
+        self.activation = nn.Sigmoid()
+
+    def forward(self, input):
+
+        test, question, tag, _, mask, interaction = input
+
+        batch_size = interaction.size(0)
+
+        # Embedding
+        embed_interaction = self.embedding_interaction(interaction)
+        embed_test = self.embedding_test(test)
+        embed_question = self.embedding_question(question)
+        embed_tag = self.embedding_tag(tag)
+
+        embed = torch.cat(
+            [
+                embed_interaction,
+                embed_test,
+                embed_question,
+                embed_tag,
+            ],
+            2,
+        )
+
+        X = self.comb_proj(embed)
+
+        out, _ = self.lstm(X)
+        out = out.contiguous().view(batch_size, -1, self.hidden_dim)
+
+        ## 이 부분이 특징적인  부분
+        extended_attention_mask = mask.unsqueeze(1).unsqueeze(2) # 차원 늘려주기
+        extended_attention_mask = extended_attention_mask.to(dtype=torch.float32)
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0 # 마스킹 된 부분 가중치 낮게 줘서 학습 안되도록
+        head_mask = [None] * self.n_layers
+
+        encoded_layers = self.attn(out, extended_attention_mask, head_mask=head_mask) # BERT Encoder를 가져다 씀
+        sequence_output = encoded_layers[-1]
+
+        out = self.fc(sequence_output).view(batch_size, -1)
+        return out
+
+
+class Bert(nn.Module):
+    def __init__(self, args):
+        super(Bert, self).__init__()
+        self.args = args
+
+        # Defining some parameters
+        self.hidden_dim = self.args.hidden_dim
+        self.n_layers = self.args.n_layers
+
+        # Embedding
+        # interaction은 현재 correct으로 구성되어있다. correct(1, 2) + padding(0)
+        self.embedding_interaction = nn.Embedding(3, self.hidden_dim // 3)
+
+        self.embedding_test = nn.Embedding(self.args.n_test + 1, self.hidden_dim // 3)
+        self.embedding_question = nn.Embedding(
+            self.args.n_questions + 1, self.hidden_dim // 3
+        )
+
+        self.embedding_tag = nn.Embedding(self.args.n_tag + 1, self.hidden_dim // 3)
+
+        # embedding combination projection
+        self.comb_proj = nn.Linear((self.hidden_dim // 3) * 4, self.hidden_dim)
+
+        # Bert config
+        self.config = BertConfig(
+            3,  # not used
+            hidden_size=self.hidden_dim,
+            num_hidden_layers=self.args.n_layers,
+            num_attention_heads=self.args.n_heads,
+            max_position_embeddings=self.args.max_seq_len,
+        )
+
+        # Defining the layers
+        # Bert Layer
+        self.encoder = BertModel(self.config) # 버트라고 되어있지만, 트랜스포머 인코더부분
+
+        # Fully connected layer
+        self.fc = nn.Linear(self.args.hidden_dim, 1)
+
+        self.activation = nn.Sigmoid()
+
+    def forward(self, input):
+        test, question, tag, _, mask, interaction = input
+        batch_size = interaction.size(0)
+
+        # 신나는 embedding
+
+        embed_interaction = self.embedding_interaction(interaction)
+
+        embed_test = self.embedding_test(test)
+        embed_question = self.embedding_question(question)
+
+        embed_tag = self.embedding_tag(tag)
+
+        embed = torch.cat( # 여기는 Continuos 없고, 범주형만 존재
+            [
+                embed_interaction,
+                embed_test,
+                embed_question,
+                embed_tag,
+            ],
+            2,
+        )
+
+        X = self.comb_proj(embed)
+
+        # Bert
+        encoded_layers = self.encoder(inputs_embeds=X, attention_mask=mask)
+        out = encoded_layers[0] # 마지막 레이어의
+
+        out = out.contiguous().view(batch_size, -1, self.hidden_dim) # 마지막 값만 뽑아서
+
+        out = self.fc(out).view(batch_size, -1) # loss 계산
+        return out
