4493aa686482f0a9f7addefefc2a3cef642c82d3
cms <chaemiinsu@gmail.com>
2022-12-02 13:24:12 +0000

    [Feat][채민수] Catboost  Feature 27개

--- CatBoost_model/catboost_main.py
+++ CatBoost_model/catboost_main.py
@@ -2,6 +2,7 @@ import torch
 import argparse
 import os
 import random
+import wandb
 
 import pandas as pd
 import numpy as np
@@ -16,6 +17,16 @@ def seed_everythings(seed):
 
 
 def main(args):
+    # wandb setting
+    # wandb.login()
+    # wandb.init(
+    #     project='Catboost',
+    #     # config=args
+    #     name='catboost_cms'
+    # )
+    # wandb.run.name = 
+    # wandb.run.save()
+
     # dataset
     dataset = CBDataset(args.data_dir)
     X_train, X_valid, y_train, y_valid = dataset.split_data()
@@ -36,6 +47,22 @@ def main(args):
     # if args.save_model:
     #     # model.save_model(args.output_dir)
     #     torch.save(model,args.output_dir)
+    # -----------------------------------------------
+    featrue_importance = model.gat_feature_importances()
+    featrue_name = model.gat_feature_names()
+    print('featrue_importance')
+    print(featrue_importance)
+    print('featrue_importance len : ', len(featrue_importance))
+    print('featrue_importance shape : ', featrue_importance.shape)
+    print(featrue_name)
+    # bs, bi, all_para, er = model.gat_()
+    # wandb.log({
+    #     # 'epoch': args.iteration,
+    #     # 'lr': args.lr,
+    #     'best_score': bs,
+    #     'best_iteration': bi,
+    #     'auc': er['validation']['AUC']
+    # })
         
 
     submission = pd.read_csv(args.inference_dir)
@@ -51,7 +78,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--output_dir", type=str, default="/opt/ml/input/code/CatBoost_model/catboost_output")
     parser.add_argument("--lr",type=float, default=0.01)
-    parser.add_argument("--iteration", type=int, default=10000) 
+    parser.add_argument("--iteration", type=int, default=1000) 
     parser.add_argument("--early_stopping", type=int, default=1000)
     parser.add_argument("--seed", type=int, default=42) # 13
     parser.add_argument("--verbose", type=int, default=100)
